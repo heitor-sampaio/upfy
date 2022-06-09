65cd7ce4f880b6b0b607d9c0b2af53b29c43fe5a
Heitor Sampaio <heitorosampaio@gmail.com>
2022-06-09 13:11:24 -0300

    Fix api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,19 @@
 import axios from 'axios';
 
-export const api = axios.create({ baseURL: 'http://localhost:3000/' });
+let url;
+
+if (process.env.NODE_ENV === 'production') {
+  if (process.env.VERCEL || process.env.NETLIFY) {
+    url = process.env.NEXT_PUBLIC_API_URL;
+  } else {
+    url = 'http://localhost:3000';
+  }
+}
+
+if (process.env.NODE_ENV === 'development') {
+  url = 'http://localhost:3000';
+}
+
+export const api = axios.create({
+  baseURL: `url`,
+});
