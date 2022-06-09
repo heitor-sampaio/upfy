4485cf05f9a7f04b7b64adb7991c8fe07e666075
Heitor Sampaio <heitorosampaio@gmail.com>
2022-06-09 14:36:52 -0300

    Fix api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,11 +3,7 @@ import axios from 'axios';
 let url;
 
 if (process.env.NODE_ENV === 'production') {
-  // if (process.env.VERCEL || process.env.NETLIFY) {
   url = process.env.NEXT_PUBLIC_API_URL;
-  // } else {
-  //   url = 'http://localhost:3000';
-  // }
 }
 
 if (process.env.NODE_ENV === 'development') {
